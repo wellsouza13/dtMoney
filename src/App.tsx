3b50d3ef47d256447f42c1ee28ea764437556c09
Wellington Pereira <wellsouza24@gmail.com>
2024-06-15 14:58:49 -0300

    fix: vite

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,12 @@ import { GlobalStyle } from "./styles/global";
 import Modal from "react-modal";
 import { NewTransactionsModal } from "./components/NewTransactionModal";
 import { TransactionsProvider } from "./hooks/useTransaction";
+import firebaseApp from './firebaseConfig';
 
 Modal.setAppElement("#root");
 
 function App() {
+  console.log(firebaseApp);
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
     useState(false);
 
