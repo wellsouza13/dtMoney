63361d4b032837966e5fa0088623fe2782322779
Wellington De Souza <wellsouza24@gmail.com>
2024-06-29 17:25:59 -0300

    chore: Update firebaseConfig.ts to use environment variables for Firebase configuration

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,18 +1,17 @@
-// firebaseConfig.ts
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAJf8TlxkW8wr44knr0WmyhRSUVWLpXoKg",
-    authDomain: "dt-money-43275.firebaseapp.com",
-    projectId: "dt-money-43275",
-    storageBucket: "dt-money-43275.appspot.com",
-    messagingSenderId: "31169185509",
-    appId: "1:31169185509:web:3799d383b874bb4f4b760c"
-  };
+  apiKey: (import.meta as any).env.VITE_FIREBASE_API_KEY,
+  authDomain: (import.meta as any).env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: (import.meta as any).env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: (import.meta as any).env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: (import.meta as any).env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: (import.meta as any).env.VITE_FIREBASE_APP_ID
+};
 
-  const app = initializeApp(firebaseConfig);
-  export const auth = getAuth(app);
-  export const googleProvider = new GoogleAuthProvider();
-  export const db = getFirestore(app);
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
